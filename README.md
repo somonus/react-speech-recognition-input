49a97335ff6d989a16b1f598a1869a9507ac4447
Andrew Golightly <support@andrewgolightly.com>
2018-11-19 23:01:20 -0700

    added restart method

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Speech To Text
 
-A speech recognition module.
-
-It's designed to listen continuously to a user (i.e. pauses are ok), and then
-converts that speech to text.
+A speech recognition module to convert speech into text.
 
 ## Install
 
@@ -14,12 +11,11 @@ converts that speech to text.
 ```
 import SpeechToText from 'speech-to-text';
 
-const onAnythingSaid = text => console.log(`Interim text: ${text}`);
 const onFinalised = text => console.log(`Finalised text: ${text}`);
-const onFinishedListening = () => console.log('Done listening.');
+const onAnythingSaid = text => console.log(`Interim text: ${text}`);
 
 try {
-  const listener = new SpeechToText(onAnythingSaid, onFinalised, onFinishedListening);
+  const listener = new SpeechToText(onFinalised, onAnythingSaid);
   listener.startListening();
 } catch (error) {
   console.log(error);
@@ -32,36 +28,45 @@ Demo [here](http://apps.golightlyplus.com/speech-to-text-demo/).
 
 ### The constructor
 
-- onAnythingSaid: this is a callback function that is called with text as it's
-  being said.
-- onFinalised: this is a callback function that is called with the full text as
-  it has been resolved in the cloud.
-- onFinishedListening: this is a callback function that is called when the
-  speech recognitions stops listening.
-- language: This is an optional string that specifies the language to be
-  interpreted as. Default is United States English. 'en-US'
+    - onFinalised - a callback that will be passed the finalised transcription from the cloud. Slow, but accuate.
+    - onAnythingSaid - Passing in this function will get the module to continuously listen. This passed in callback will be passed interim transcriptions. These transcriptions are generated quickly, but are less accurate than the finalised text.
+    - language - the language to interpret against. Default is US English.
 
-The constructor will throw an error if speech recognition is not supported by
-the browser.
+The constructor will throw an error if speech recognition is not supported by the browser.
 
 ```
-if (
-  !('webkitSpeechRecognition' in window) &&
-  !('SpeechRecognition' in window)
-) {
-  throw new Error("This browser doesn't support speech recognition. Try Google Chrome or Firefox.");
-}
+    // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition#Browser_compatibility
+    if (!('webkitSpeechRecognition' in window)) {
+      throw new Error(
+        "This browser doesn't support speech recognition. Try Google Chrome."
+      );
+    }
 ```
 
 ### startListening
 
-Initiates listening to speech input and will continue to call the callback
-functions provided until the speech recognition stops listening. After which
-you'll need to call this function again.
+Initiates listening to speech input.
 
 ### stopListening
 
 Does just that. Stops listening.
+
+### restart
+
+This will restart the speech recognition service.
+
+motivation: There is a known issue where the speech recognition service randomly stops listening. The simplest solution is to restart the service. Implementation inspired by [this thread](https://stackoverflow.com/a/40676839/2813041).
+
+So for robustness, maybe you want to restart the speech recognition in your app every 11 seconds. You could do it with this code
+
+```
+const listener = new SpeechToText(onFinalised, onAnythingSaid);
+listener.startListening();
+
+setInterval(() => {
+  listener.restart();
+}, 11000);
+```
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
 export default class SpeechToText {
   /*
-    Arguments for the constructor.
-  
-    onAnythingSaid - a callback that will be passed interim transcriptions
-    (fairly immediate, less accurate)
-  
-    onFinalised - a callback that will be passed the finalised transcription from the cloud
-    (slow, much more accuate)
-  
-    onFinishedListening - a callback that will be called when the speech recognition stops listening
-  
-    language - the language to interpret against. Default is US English.
+    Arguments for the constructor:
+
+    - onFinalised - a callback that will be passed the finalised transcription from the cloud. Slow, but accuate.
+    - onAnythingSaid - a callback that will be passed interim transcriptions. Fairly immediate, but less accurate than finalised text.
+    - language - the language to interpret against. Default is US English.
+
     */
-  constructor(
-    onAnythingSaid,
-    onFinalised,
-    onFinishedListening,
-    language = 'en-US'
-  ) {
+  constructor(onFinalised, onAnythingSaid, language = 'en-US') {
     // Check to see if this browser supports speech recognition
-    if (
-      !('webkitSpeechRecognition' in window) &&
-      !('SpeechRecognition' in window)
-    ) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition#Browser_compatibility
+    if (!('webkitSpeechRecognition' in window)) {
       throw new Error(
-        "This browser doesn't support speech recognition. Try Google Chrome or Firefox."
+        "This browser doesn't support speech recognition. Try Google Chrome."
       );
     }
 
-    const SpeechRecognition =
-      window.webkitSpeechRecognition || window.SpeechRecognition;
+    let continuouslyListen = !!onAnythingSaid;
+
+    const SpeechRecognition = window.webkitSpeechRecognition;
     this.recognition = new SpeechRecognition();
 
-    //  Keep listening even if the speaker pauses, and return interim results.
-    this.recognition.continuous = true;
-    this.recognition.interimResults = true;
+    //  if to continuously listen, get interim results too
+    this.recognition.continuous = continuouslyListen;
+    this.recognition.interimResults = continuouslyListen;
     this.recognition.lang = language;
 
     let finalTranscript = '';
@@ -51,22 +40,41 @@ export default class SpeechToText {
           finalTranscript += transcriptionPiece;
           onFinalised(finalTranscript);
           finalTranscript = '';
-        } else {
+        } else if (continuouslyListen) {
           interimTranscript += transcriptionPiece;
           onAnythingSaid(interimTranscript);
         }
       }
     };
 
-    this.recognition.onend = () => onFinishedListening();
+    // if speech recognition ends, and it's flagged to restart, then restart!
+    this.recognition.onend = () => {
+      if (this.shouldRestart) {
+        this.startListening();
+        this.shouldRestart = false;
+      }
+    };
+
+    this.recognition.onerror = evt => {
+      console.log(
+        `Error with speech recognition: ${evt.error}. Restarting service...`
+      );
+
+      this.restart();
+    };
+  }
+
+  startListening() {
+    this.recognition.start();
   }
 
   stopListening() {
     this.recognition.stop();
   }
 
-  // start listening!
-  startListening() {
-    this.recognition.start();
+  // to restart the speech recognition engine, stop it, then flag that it should restart.
+  restart() {
+    this.stopListening();
+    this.shouldRestart = true;
   }
 }
